46dc96e8b30eb18dc0d73832ba9be560bbb03822
nnecec <nnecec@outlook.com>
2024-08-26 00:12:11 +0800

    --wip-- [skip ci]

--- src-tauri/src/commands/gitlab.rs
+++ src-tauri/src/commands/gitlab.rs
@@ -5,20 +5,20 @@ use tauri::{AppHandle, Manager, Wry};
 use tauri_plugin_store::{with_store, StoreCollection};
 
 #[tauri::command]
-pub fn fuzzy_gitlab_search(app_handle: &AppHandle, text: String) {
+pub fn fuzzy_gitlab_search(app_handle: AppHandle, text: String) {
     println!("I was invoked from JavaScript, with this message: {}", text);
 
-    let stores = app_handle
-        .try_state::<StoreCollection<Wry>>()
-        .ok_or("Store not found")?;
+    let stores = app_handle.state::<StoreCollection<Wry>>();
 
     let path = PathBuf::from("setting.bin");
 
-    with_store(app_handle.clone(), stores, path, |store| {
+    let setting = with_store(app_handle.clone(), stores, path, |store| {
         // Get a value from the store.
+
         let value = store.get("gitlab").expect("Failed to get value from store");
-        println!("{}", value);
+        println!("value {} host {}", value, value.get("host").unwrap());
 
         Ok(())
-    });
+    })
+    .unwrap();
 }
