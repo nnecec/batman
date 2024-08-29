9370a6fb7a894bf162fe67bd180b3ae8e7a8443f
nnecec <nnecec@outlook.com>
2024-08-30 01:29:51 +0800

    --wip-- [skip ci]

--- src-tauri/src/commands/gitlab.rs
+++ src-tauri/src/commands/gitlab.rs
@@ -12,37 +12,34 @@ struct Project {
     name: String,
 }
 
-#[tauri::command]
-pub fn fuzzy_gitlab_search(app_handle: AppHandle, text: String) {
-    println!("I was invoked from JavaScript, with this message: {}", text);
-
+fn get_gitlab_client(app_handle: AppHandle) -> Result<Gitlab, GitlabError> {
     let stores = app_handle.state::<StoreCollection<Wry>>();
 
     let path = PathBuf::from("setting.bin");
+    with_store(app_handle.clone(), stores, path, |store| {
+        let setting_store = store.get("gitlab").expect("Failed to get value from store");
 
-    let setting = with_store(app_handle.clone(), stores, path, |store| {
-        let value = store.get("gitlab").expect("Failed to get value from store");
+        let host = setting_store["host"].as_str().unwrap();
+        let access_token = setting_store["accessToken"].as_str().unwrap();
 
-        let host = value["host"].as_str().unwrap();
-        let access_token = value["accessToken"].as_str().unwrap();
-
-        let client = match Gitlab::new(host, access_token) {
-            Ok(client) => {
-                let pageable_endpoint = projects::Projects::builder()
-                    .order_by(projects::ProjectOrderBy::UpdatedAt)
-                    .sort(api::common::SortOrder::Descending)
-                    .build()
-                    .unwrap();
-                let first_page: Vec<Project> = pageable_endpoint.query(&client).unwrap();
-
-                println!("first_page {:?}", first_page);
-            }
-            Err(error) => {
-                println!("error {:?}", error);
-            }
-        };
-
-        Ok(())
+        match Gitlab::new(host, access_token) {
+            Ok(client) => Ok(client),
+            Err(err) => Err(err),
+        }
     })
-    .unwrap();
+}
+
+#[tauri::command]
+pub fn fuzzy_gitlab_search(app_handle: AppHandle, text: String) {
+    println!("I was invoked from JavaScript, with this message: {}", text);
+    let client = get_gitlab_client(app_handle).unwrap();
+
+    let pageable_endpoint = projects::Projects::builder()
+        .order_by(projects::ProjectOrderBy::UpdatedAt)
+        .sort(api::common::SortOrder::Descending)
+        .build()
+        .unwrap();
+    let first_page: Vec<Project> = pageable_endpoint.query(&client).unwrap();
+
+    println!("first_page {:?}", first_page);
 }
