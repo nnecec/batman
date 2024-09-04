a55f3920980a259682c48b350cbeba7e1beeb10b
nnecec <nnecec@outlook.com>
2024-09-05 00:46:46 +0800

    --wip-- [skip ci]

--- src-tauri/src/commands/gitlab.rs
+++ src-tauri/src/commands/gitlab.rs
@@ -1,45 +1,62 @@
-use std::path::PathBuf;
+use std::f32::consts::E;
 
-use gitlab::api::{self, groups, projects, Query};
 use gitlab::Gitlab;
 use serde::Deserialize;
 use serde_json::json;
 use tauri::{AppHandle, Manager, Wry};
-use tauri_plugin_store::{with_store, StoreCollection};
+use tauri_plugin_store::{Store, StoreBuilder};
 
-#[derive(Debug, Deserialize)]
-struct Project {
-    name: String,
+#[derive(Debug, Clone)]
+pub struct GitlabSetting {
+    pub host: String,
+    pub access_token: String,
 }
 
-fn get_gitlab_client(app_handle: AppHandle) -> Result<Gitlab, GitlabError> {
-    let stores = app_handle.state::<StoreCollection<Wry>>();
+fn load_from_store<R: tauri::Runtime>(
+    store: &Store<R>,
+) -> Result<GitlabSetting, Box<dyn std::error::Error>> {
+    let host = store
+        .get("gitlab.host")
+        .and_then(|v| v.as_str())
+        .map(|s| s.to_string())
+        .unwrap_or_else(|| "".to_string());
 
-    let path = PathBuf::from("setting.bin");
-    with_store(app_handle.clone(), stores, path, |store| {
-        let setting_store = store.get("gitlab").expect("Failed to get value from store");
+    let access_token = store
+        .get("gitlab.accessToken")
+        .and_then(|v| v.as_str())
+        .map(|s| s.to_string())
+        .unwrap_or_else(|| "".to_string());
 
-        let host = setting_store["host"].as_str().unwrap();
-        let access_token = setting_store["accessToken"].as_str().unwrap();
+    Ok(GitlabSetting { host, access_token })
+}
 
-        match Gitlab::new(host, access_token) {
-            Ok(client) => Ok(client),
-            Err(err) => Err(err),
+fn get_gitlab_client(app_handle: AppHandle) {
+    let mut store = StoreBuilder::new("setting.bin").build(app_handle.clone());
+
+    let _ = store.load();
+
+    let settings = load_from_store(&store);
+    match settings {
+        Ok(settings) => Gitlab::new(settings.host, settings.access_token),
+        Err(err) => {
+            eprintln!("Error loading settings: {}", err);
+            // Handle the error case if needed
+            Err(err)
         }
-    })
+    }
 }
 
 #[tauri::command]
 pub fn fuzzy_gitlab_search(app_handle: AppHandle, text: String) {
     println!("I was invoked from JavaScript, with this message: {}", text);
-    let client = get_gitlab_client(app_handle).unwrap();
+    let client = get_gitlab_client(app_handle);
 
-    let pageable_endpoint = projects::Projects::builder()
-        .order_by(projects::ProjectOrderBy::UpdatedAt)
-        .sort(api::common::SortOrder::Descending)
-        .build()
-        .unwrap();
-    let first_page: Vec<Project> = pageable_endpoint.query(&client).unwrap();
+    // let pageable_endpoint = projects::Projects::builder()
+    //     .order_by(projects::ProjectOrderBy::UpdatedAt)
+    //     .sort(api::common::SortOrder::Descending)
+    //     .build()
+    //     .unwrap();
+    // let first_page: Vec<Project> = pageable_endpoint.query(&client).unwrap();
 
-    println!("first_page {:?}", first_page);
+    // println!("first_page {:?}", first_page);
 }
