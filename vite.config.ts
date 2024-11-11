a9f8113b0035f4699c23031bd85e121b1b1bd1a5
nnecec <nnecec@outlook.com>
2024-11-11 14:15:05 +0800

    feat: update deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
+import path from 'node:path'
+
+import generouted from '@generouted/react-router/plugin'
 import { defineConfig } from 'vite'
-import path from 'path'
 
 import react from '@vitejs/plugin-react'
-import generouted from '@generouted/react-router/plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
